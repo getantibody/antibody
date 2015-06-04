f39d8fea818a25ddb0952a406e35b830ba357815
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-06-04 20:26:42 -0300

    support for stdin

--- antibody.zsh
+++ antibody.zsh
@@ -4,5 +4,11 @@ ANTIBODY_HOME="$(dirname $0)"
 mkdir -p "$HOME/.antibody" || true
 
 antibody() {
-  source "$(${ANTIBODY_HOME}/antibody $@)"/*.plugin.zsh
+  # if [ -f "$1" ]; then
+  #   cat "$1" | while read plugin; do
+  #     source "$(${ANTIBODY_HOME}/antibody $plugin)"/*.plugin.zsh
+  #   done
+  # else
+    source "$(${ANTIBODY_HOME}/antibody $*)"/*.plugin.zsh
+  # fi
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"strings"
@@ -9,12 +10,10 @@ import (
 
 const GH = "https://github.com/"
 
-func clone(home string) string {
-	repo := os.Args[1:][0]
-	folder := home + strings.Replace(repo, "/", "-", -1)
+func clone(bundle string, home string) string {
+	folder := home + strings.Replace(bundle, "/", "-", -1)
 	if _, err := os.Stat(folder); os.IsNotExist(err) {
-		// fmt.Println("Cloning", repo)
-		clone := exec.Command("git", "clone", "--depth", "1", GH+repo, folder)
+		clone := exec.Command("git", "clone", "--depth", "1", GH+bundle, folder)
 		clone.Start()
 	}
 	return folder
@@ -22,6 +21,15 @@ func clone(home string) string {
 
 func main() {
 	home := os.Getenv("HOME") + "/.antibody/"
-	folder := clone(home)
-	fmt.Println(folder)
+	stat, _ := os.Stdin.Stat()
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		bundles, _ := ioutil.ReadAll(os.Stdin)
+		for _, bundle := range strings.Split(string(bundles), "\n") {
+			if bundle != "" {
+				fmt.Println(clone(bundle, home))
+			}
+		}
+	} else {
+		fmt.Println(clone(os.Args[1:][0], home))
+	}
 }
