007908da55d7cd3eaa35dea6eadcfd7780edff8f
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-06-05 21:13:18 -0300

    standarized test names

--- api_test.go
+++ api_test.go
@@ -17,29 +17,29 @@ func home() string {
 	return file + "/"
 }
 
-func Test_ProcessesArgsBunde(t *testing.T) {
+func TestProcessesArgsBunde(t *testing.T) {
 	home := home()
 	ProcessArgs([]string{"bundle", "caarlos0/zsh-pg"}, home)
 }
 
-func Test_Update(t *testing.T) {
+func TestUpdate(t *testing.T) {
 	home := home()
 	ProcessArgs([]string{"update"}, home)
 }
 
-func Test_bundlesSinglePlugin(t *testing.T) {
+func TestBundlesSinglePlugin(t *testing.T) {
 	home := home()
 	Bundle("caarlos0/zsh-pg", home)
 }
 
-func Test_loadsDefaultHome(t *testing.T) {
+func TestLoadsDefaultHome(t *testing.T) {
 	os.Unsetenv("ANTIBODY_HOME")
 	if !strings.HasSuffix(Home(), "/.antibody/") {
 		t.Error("Expected default ANTIBODY_HOME")
 	}
 }
 
-func Test_loadsCustomHome(t *testing.T) {
+func TestLoadsCustomHome(t *testing.T) {
 	home := home()
 	if home != Home() {
 		t.Error("Expected custom ANTIBODY_HOME")
