221488d8f90f5c9de95a474b3bfa53ae73aaa527
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-06-06 01:26:05 -0300

    improved release script

--- release.sh
+++ release.sh
@@ -5,8 +5,8 @@ echo "Creating release $1..."
 go build
 
 go get golang.org/x/tools/cmd/cover
-go test -cover
+go test -v -cover
 
-tar -cvzf "antibody-$RELEASE.tar.gz" antibody antibody.zsh
+tar -cvzf "antibody-$RELEASE-$(uname -s).tar.gz" antibody antibody.zsh
 git tag "$RELEASE"
 git push origin "$RELEASE"
