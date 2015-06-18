5d06dae0c78d1655fc243183f96d8a0a7a3cc197
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-06-17 21:55:05 -0300

    new release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,44 @@
 #!/bin/bash
-RELEASE="$1"
+RELEASE="v$1"
+CURRENT="$(git describe --tags)"
 echo "Creating release $1..."
 
-go build
+#go get golang.org/x/tools/cmd/cover
+go test -v -cover ./lib
 
-go get golang.org/x/tools/cmd/cover
-go test -v -cover
+# go get github.com/mitchellh/gox
+# gox -build-toolchain
+gox -osarch="linux/amd64 darwin/amd64" ./...
 
-tar -cvzf "antibody-$RELEASE-$(uname -s).tar.gz" antibody antibody.zsh
 git tag "$RELEASE"
 git push origin "$RELEASE"
+
+LOG="$(git log --pretty=oneline --abbrev-commit "$CURRENT"..HEAD)"
+
+# go get github.com/aktau/github-release
+github-release release \
+  --user caarlos0 \
+  --repo antibody \
+  --tag "$RELEASE" \
+  --name "$2" \
+  --description "$LOG" \
+  --pre-release
+
+if [ "$(uname -s)" = "Darwin" ]; then
+  alias tar="gtar"
+fi
+
+for platform in Darwin Linux; do
+  filename="antibody-$RELEASE-$platform.tar.gz"
+  platform_lower="$(echo $platform | tr '[:upper:]' '[:lower:]')"
+  tar \
+    --transform="s/_${platform_lower}_amd64//" \
+    -cvzf "$filename" \
+    "antibody_${platform_lower}_amd64" antibody.zsh
+  github-release upload \
+    --user caarlos0 \
+    --repo antibody \
+    --tag "$RELEASE" \
+    --name "$filename" \
+    --file "$filename"
+done
