69f4fb811bef7c1d90ab0b8b2988e718fc715071
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-06-18 21:17:03 -0300

    organized relase script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 RELEASE="v$1"
 CURRENT="$(git tag | tail -n1)"
-echo "Creating release $1..."
 
-#go get golang.org/x/tools/cmd/cover
-go test -v -cover ./lib
-
+# echo "Installing needed tools..."
 # go get github.com/mitchellh/gox
 # gox -build-toolchain
+# go get github.com/aktau/github-release
+# go get golang.org/x/tools/cmd/cover
+
+echo "Creating release $1..."
+go test -v -cover ./lib
 gox \
   -output="./bin/{{.Dir}}_{{.OS}}_{{.Arch}}" \
   -osarch="linux/amd64 darwin/amd64" \
   ./...
-
 git tag "$RELEASE"
 git push origin "$RELEASE"
-
 LOG="$(git log --pretty=oneline --abbrev-commit "$CURRENT"..HEAD)"
-
-go get github.com/aktau/github-release
 github-release release \
   --user caarlos0 \
   --repo antibody \
@@ -26,11 +24,9 @@ github-release release \
   --name "$2" \
   --description "$LOG" \
   --pre-release
-
 if [ "$(uname -s)" = "Darwin" ]; then
   PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 fi
-
 for platform in Darwin Linux; do
   filename="antibody-$RELEASE-$platform.tar.gz"
   platform_lower="$(echo $platform | tr '[:upper:]' '[:lower:]')"
