9a5ee88b2bb84a2955c6f694767392ecc91355af
Esko <esko@esko.bar>
2016-02-24 17:49:00 +0100

    Append OS and architecture to binary path

--- antibody.zsh
+++ antibody.zsh
@@ -1,15 +1,17 @@
 #!/usr/bin/env zsh
 ANTIBODY_BINARIES="$(dirname $0)"
+OPERATING_SYSTEM="$(uname -s)"
+ARCHITECTURE="$(uname -m)"
 
 antibody() {
   case "$1" in
   bundle|update)
     while read bundle; do
       source "$bundle" 2&> /tmp/antibody-log
-    done < <( ${ANTIBODY_BINARIES}/bin/antibody $@ )
+    done < <( "${ANTIBODY_BINARIES}/bin/antibody-${OPERATING_SYSTEM}-${ARCHITECTURE}" $@ )
     ;;
   *)
-    ${ANTIBODY_BINARIES}/bin/antibody $@
+    "${ANTIBODY_BINARIES}/bin/antibody-${OPERATING_SYSTEM}-${ARCHITECTURE}" $@
     ;;
   esac
 }
