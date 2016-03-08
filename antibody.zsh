ce7e412f95abcfe75d42940f4fcfa6245430663d
Esko <esko@esko.bar>
2016-03-08 22:31:41 +0100

    Rename ANTIBODY_BINARIES to ANTIBODY_DIRECTORY

--- antibody.zsh
+++ antibody.zsh
@@ -1,15 +1,15 @@
 #!/usr/bin/env zsh
-ANTIBODY_BINARIES="$(dirname $0)"
+ANTIBODY_DIRECTORY="$(dirname $0)"
 OPERATING_SYSTEM="$(uname -s)"
 ARCHITECTURE="$(uname -m)"
 
 antibody() {
-  antibody_os_arch=${ANTIBODY_BINARIES}/bin/antibody-${OPERATING_SYSTEM}-${ARCHITECTURE}
+  antibody_os_arch=${ANTIBODY_DIRECTORY}/bin/antibody-${OPERATING_SYSTEM}-${ARCHITECTURE}
 
   if [[ -x "$antibody_os_arch" ]]; then
     antibody="$antibody_os_arch"
   else
-    antibody="${ANTIBODY_BINARIES}/bin/antibody"
+    antibody="${ANTIBODY_DIRECTORY}/bin/antibody"
   fi
 
   case "$1" in
