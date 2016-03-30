811cf599b887c4d82223ee85ef6273ea8b1d4eab
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-03-30 19:00:34 -0300

    test fix

--- shell/init_test.go
+++ shell/init_test.go
@@ -1,16 +1,15 @@
 package shell_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/getantibody/antibody/shell"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestClonesRepo(t *testing.T) {
+func TestGeneratesInit(t *testing.T) {
 	shell, err := shell.Init()
 	assert.Nil(t, err)
 	assert.NotNil(t, shell)
-	fmt.Println(shell)
+	assert.NotEmpty(t, shell)
 }
