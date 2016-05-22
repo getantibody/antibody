86be8dc9f9b1d3a24d8c2bb85bb71af30793e3a0
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-05-22 12:54:01 -0300

    improvements

--- scripts/profiling/work.sh
+++ scripts/profiling/work.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
+
+open_pdfs() {
+  for i in cpu mem block; do
+    go tool pprof --pdf antibody "$i.pprof" > "$1_$i.pdf"
+    open "$1_$i.pdf"
+  done
+}
+
 git apply "./scripts/profiling/patch.patch"
 go build -o antibody ./cmd/antibody
 export ANTIBODY_HOME="$(mktemp -d)"
 # bundle all plugins from awesome-zsh-plugins
 ./antibody bundle < ./scripts/profiling/bundles.txt
-for i in cpu mem block; do
-  go tool pprof --pdf antibody "$i.pprof" > "$i.pdf"
-  open "$i.pdf"
-done
+open_pdfs bundle
+./antibody update
+open_pdfs updated
+./antibody list
+open_pdfs list
+./antibody home
+open_pdfs home
+./antibody init
+open_pdfs init
+
 rm -f ./antibody
 git checkout ./cmd/antibody/main.go
