b579b887a167971465389b7d9d510a13bc4d7ac1
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-05-22 12:29:34 -0300

    fixing profiling

--- scripts/profiling/work.sh
+++ scripts/profiling/work.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-current="$(dirname $0)"
-git apply "$current/patch.patch"
+git apply "./scripts/profiling/patch.patch"
 go build -o antibody ./cmd/antibody
 export ANTIBODY_HOME="$(mktemp -d)"
 # bundle all plugins from awesome-zsh-plugins
@@ -9,5 +8,5 @@ for i in cpu mem block; do
   go tool pprof --pdf antibody "$i.pprof" > "$i.pdf"
   open "$i.pdf"
 done
-rm -rf ./antibody
+rm -f ./antibody
 git checkout ./cmd/antibody/main.go
