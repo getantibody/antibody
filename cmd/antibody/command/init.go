3e77cf095f3e940410eae0d6d6bc484c642e0a2b
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-08-07 14:07:48 -0300

    minor typo fix

--- cmd/antibody/command/init.go
+++ cmd/antibody/command/init.go
@@ -10,7 +10,7 @@ import (
 // Init prints out the antibody's shell init script
 var Init = cli.Command{
 	Name:  "init",
-	Usage: "Initializes the shell so Antibody can work as expected.",
+	Usage: "Initializes the shell so Antibody can work as expected",
 	Action: func(ctx *cli.Context) error {
 		fmt.Println(shell.Init())
 		return nil
