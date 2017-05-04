a7e2317c1ba9591ae3e358e0b497f8f0cc0ce8c6
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-05-04 20:08:37 -0300

    finished

---
+++ antibodylib/info.go
@@ -0,0 +1,4 @@
+package antibodylib
+
+// Version is antibody's version
+var Version = "dev"
---
+++ cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/getantibody/antibody/antibodylib"
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "shows current version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("antibody version %v\n", antibodylib.Version)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
